892f8159f2b266c345a01539b5ce00e11ac03d52
А <116899834+annistasiuk@users.noreply.github.com>
2025-12-05 09:41:26 +0200

    Delete sto_management directory

--- sto_management/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('cars/', include('cars.urls')),
-    path('repairs/', include('repairs.urls')),
-]
